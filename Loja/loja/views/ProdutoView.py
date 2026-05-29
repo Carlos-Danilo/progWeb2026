44e2aab2766b2d84527c25c2ea3fb187527972f6
Carlos-Danilo <carlos.danilo@escolar.ifrn.edu.br>
2026-05-29 11:37:03 -0300

    oi

--- Loja/loja/views/ProdutoView.py
+++ Loja/loja/views/ProdutoView.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from loja.models import Produto
 from datetime import timedelta, datetime
 from django.utils import timezone
@@ -10,8 +10,18 @@ def list_produto_view(request, id=None):
     categoria = request.GET.get("categoria")
     fabricante = request.GET.get("fabricante")
     dias = request.GET.get("dias")
-    produtos = Produto.objects.filter(Produto=produto)
-    #print(produtos)
+
+    if destaque is not None:
+        print(destaque)
+    if produto is not None:
+        print(produto)
+    if promocao is not None:
+        print(promocao)
+    if categoria is not None:
+        print(categoria)
+    if fabricante is not None:
+        print(fabricante)
+
     produtos = Produto.objects.all()
     if dias is not None:
         now = timezone.now()
@@ -31,4 +41,55 @@ def list_produto_view(request, id=None):
         produtos = produtos.filter(id=id)
     print(produtos)
     print(f"ID recebido: {id}")
-    return HttpResponse('<h1>Produto de id %s!</h1>' % id)
+    context = {
+        'produtos': produtos
+    }
+    return render(request, template_name='produto/produto.html', context=context, status=200)
+
+
+def edit_produto_view(request, id=None):
+    produtos = Produto.objects.all()
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    produto = produtos.first()
+    print(produto)
+    context = {'produto': produto}
+    return render(request, template_name='produto/produto-edit.html', context=context, status=200)
+
+
+def edit_produto_postback(request, id=None):
+    if request.method == 'POST':
+        id = request.POST.get("id")
+        produto = request.POST.get("Produto")
+        destaque = request.POST.get("destaque")
+        promocao = request.POST.get("promocao")
+        msgPromocao = request.POST.get("msgPromocao")
+        print("postback")
+        print(id)
+        print(produto)
+        print(destaque)
+        print(promocao)
+        print(msgPromocao)
+        try:
+            obj_produto = Produto.objects.filter(id=id).first()
+            obj_produto.Produto = produto
+            obj_produto.destaque = (destaque is not None)
+            obj_produto.promocao = (promocao is not None)
+            if msgPromocao is not None:
+                obj_produto.msgPromocao = msgPromocao
+            obj_produto.save()
+            print("Produto %s salvo com sucesso" % produto)
+        except Exception as e:
+            print("Erro salvando edição de produto: %s" % e)
+    return redirect("/produto")
+
+
+def details_produto_view(request, id=None):
+    # Processa o evento GET gerado pela action
+    produtos = Produto.objects.all()
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    produto = produtos.first()
+    print(produto)
+    context = {'produto': produto}
+    return render(request, template_name='produto/produto-details.html', context=context, status=200)
